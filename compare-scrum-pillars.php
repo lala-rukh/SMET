5d0f2b28d4bd665d2a051957fc3716a9789434ea
lala-rukh-confiz <lala.rukh@confiz.com>
2017-02-22 22:38:07 +0500

    Adding database save code and modifying scrum pillars form

--- compare-scrum-pillars.php
+++ compare-scrum-pillars.php
@@ -75,70 +75,71 @@
 			
 			<div class="row contact-wrap"> 
                 <div class="status alert alert-success" style="display: none"></div>
-                <form id="main-contact-form" class="contact-form" name="contact-form" method="post" action="sendemail.php">
+                <form id="compare-scrum-pillars-form" method="post" action="save-scrum-pillars-data.php">
                     <div class="col-sm-6">
                         <div class="form-group">
                             <label>Do you have any established process in your organization? If yes, please provide name and details.</label>
-                            <input type="text" name="name" class="form-control" required="required">
+                            <input type="text" name="process_in_organization" class="form-control" required="required">
                         </div>
 						<div class="form-group">
                             <label>How many number of figures do you have for that process in your organization?</label>
-							<input type="number" name="name" class="form-control" required="required">
+							<input type="number" name="no_of_figures_process" class="form-control" required="required">
                         </div>
                         <div class="form-group">
                             <label>How many number of sections do you have for that process in your organization?</label>
-                            <input type="number" name="name" class="form-control" required="required">
+                            <input type="number" name="no_of_sections_process" class="form-control" required="required">
                         </div>
                         <div class="form-group">
                             <label>How many number of tables do you have for that process in your organization?</label>
-							<input type="number" name="name" class="form-control" required="required">
+							<input type="number" name="no_of_tables_process" class="form-control" required="required">
                         </div>   
                         <div class="form-group">
                             <label>Is conflict resolution process shared within the organization?</label>
-                            <select class="form-control">
-                              <option value="">Yes</option>
-                              <option value="">No</option>
+                            <select class="form-control" name="conflict_resolution_process_exists">
+                              <option value="1">Yes</option>
+                              <option value="0">No</option>
                             </select>
                         </div>						
                     </div>
                     <div class="col-sm-6">
                         <div class="form-group">
                             <label>Do definition of done exists?</label>
-                            <select class="form-control">
-							  <option value="">Yes</option>
-                              <option value="">No</option>
+                            <select class="form-control" name="def_of_done">
+							  <option value="1">Yes</option>
+                              <option value="0">No</option>
 							</select>
                         </div>
                        <div class="form-group">
                             <label>How definition of done communicated to project team?</label>
-                            <select class="form-control">
-                              <option value="">Written Wiki</option>
-                              <option value="">Word of mouth</option>
-                              <option value="">Scrum board</option>
+                            <select class="form-control" name="def_of_done_communication">
+                              <option value="Written Wiki">Written Wiki</option>
+                              <option value="Word of mouth">Word of mouth</option>
+                              <option value="Scrum board">Scrum board</option>
                             </select>
                         </div>
 						<div class="form-group">
                             <label>How do you monitor and inspect progress towards Sprint Goal?</label>
-                            <select class="form-control">
-                              <option value="">Burn-down chart</option>
-                              <option value="">Burn-up charts</option>
-                              <option value="">Sprint tracking sheets</option>
-                              <option value="">Touch base</option>
+                            <select class="form-control" name="progress_sprint_goal">
+                              <option value="Burn-down chart">Burn-down chart</option>
+                              <option value="Burn-up charts">Burn-up charts</option>
+                              <option value="Sprint tracking sheets">Sprint tracking sheets</option>
+                              <option value="Touch base">Touch base</option>
                             </select>
                         </div>
                         <div class="form-group">
                             <label>To what extent Scrum is adapted to your organizational and project condition?</label>
-                            <textarea name="taname" id="taid" rows="4" wrap="soft" class="form-control"></textarea>
+                            <textarea name="scrum_adapted" id="taid" rows="4" wrap="soft" class="form-control"></textarea>
                         </div>
                     </div>
                     <div class="col-sm-12">
 						<div class="form-group" style="float:left;">
-                            <!--<button type="submit" name="submit" class="btn btn-primary btn-lg" required="required">Next</button>-->
+                            <!-- <button type="submit" name="submit" class="btn btn-primary btn-lg" required="required">Previous</button> -->
 							<a href="compare-project-data.php" class="btn btn-primary btn-lg">Previous</a>
                         </div>
 						<div class="form-group" style="float:right;">
-                            <!--<button type="submit" name="submit" class="btn btn-primary btn-lg" required="required">Next</button>-->
-							<a href="compare-scrum-teams.php" class="btn btn-primary btn-lg">Next</a>
+<!--                             <input type="submit" value="Search">
+ -->                            <button type="submit" name="submit" class="btn btn-primary btn-lg" required="required">Next</button>
+							<!-- <a href="compare-scrum-teams.php" class="btn btn-primary btn-lg">Next</a> -->
                         </div>
 					</div>
                 </form> 
---
+++ save-scrum-pillars-data.php
@@ -0,0 +1,43 @@
+<?php
+    $process_in_organization = $_POST['process_in_organization']; 
+    $no_of_figures_process = $_POST['no_of_figures_process']; 
+    $no_of_sections_process = $_POST['no_of_sections_process']; 
+    $no_of_tables_process = $_POST['no_of_tables_process'];
+    $conflict_resolution_process_exists = $_POST['conflict_resolution_process_exists']; 
+    $def_of_done = $_POST['def_of_done']; 
+    $def_of_done_communication = $_POST['def_of_done_communication']; 
+    $progress_sprint_goal = $_POST['progress_sprint_goal'];
+    $scrum_adapted = $_POST['scrum_adapted'];
+    
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "smet_core_database";
+
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        // set the PDO error mode to exception
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        $sth = $conn->prepare("INSERT INTO scrum_pillars(project_id, is_processes_exist, no_of_figures_process, no_of_tables_process, no_of_sections_process, is_process_shared, is_def_of_done_exists, communication_def_of_done, inspect_progress_sprint_goal, scrum_adapted, is_deleted, created_date) 
+        VALUES ('12345',:process_in_organization,:no_of_figures_process,:no_of_tables_process,:no_of_sections_process,:conflict_resolution_process_exists,:def_of_done,:def_of_done_communication,:progress_sprint_goal,:scrum_adapted,0,now())");
+        $sth->bindParam(':process_in_organization', $process_in_organization, PDO::PARAM_INT);
+        $sth->bindParam(':no_of_figures_process', $no_of_figures_process, PDO::PARAM_INT);
+        $sth->bindParam(':no_of_tables_process', $no_of_tables_process, PDO::PARAM_INT);
+        $sth->bindParam(':no_of_sections_process', $no_of_sections_process, PDO::PARAM_INT);
+        $sth->bindParam(':conflict_resolution_process_exists', $conflict_resolution_process_exists, PDO::PARAM_INT);
+        $sth->bindParam(':def_of_done', $def_of_done, PDO::PARAM_INT);
+        $sth->bindParam(':def_of_done_communication', $def_of_done_communication, PDO::PARAM_STR, 12);
+        $sth->bindParam(':progress_sprint_goal', $progress_sprint_goal, PDO::PARAM_STR, 12);
+        $sth->bindParam(':scrum_adapted', $scrum_adapted, PDO::PARAM_STR, 12);
+        $sth->execute();
+
+        echo "New record created successfully";
+        }
+    catch(PDOException $e)
+        {
+        echo $sql . "<br>" . $e->getMessage();
+        }
+
+    $conn = null;
+?>
